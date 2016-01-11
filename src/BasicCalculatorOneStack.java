c0f5cf568e3e085b1b2b9155847d0c8e80ae7ee5
Ramona Comanescu <com.ramona@yahoo.com>
2016-01-11 13:47:30 +0000

    Create BasicCalculatorOneStack.java

---
+++ src/BasicCalculatorOneStack.java
@@ -0,0 +1,43 @@
+public class BasicCalculatorOneStack {
+    public int calculate(String s) {
+        Stack<Integer> st=new Stack<Integer>();
+        int res=0,val=0;
+        int sign=1;
+        for(int i=0;i<s.length();i++){
+            Character c=s.charAt(i);
+            if(c.equals('(')){
+                st.push(sign);
+            }
+            else if(c.equals(')')){
+                st.pop();
+            }
+            else if(c.equals('+')||c.equals('-')){
+                res+=sign*val;
+                val=0;
+                if(!st.isEmpty()){
+                    if(c.equals('-')){
+                        sign=st.peek()*(-1);
+                    }
+                    else
+                    {
+                        sign=st.peek();
+                    }
+                    
+                }
+                    
+                else {
+                        if(c.equals('-'))
+                        sign=-1;
+                        else
+                        sign=1;
+                    
+                 }
+            }
+            else if(!c.equals(' ')){
+                val=val*10+(c-'0');
+            }
+        }
+        res+=val*sign;
+        return res;
+    }
+}
