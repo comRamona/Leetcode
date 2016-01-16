5475aa02fdd5a13c29cc01a0b24d7e9b6b6dfb01
Ramona Comanescu <com.ramona@yahoo.com>
2016-01-16 17:39:36 +0000

    Update 3Sum.java

--- src/3Sum.java
+++ src/3Sum.java
@@ -1,6 +1,7 @@
 /**
 Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets
 in the array which gives the sum of zero.
+n^2 complexity
 */
 
 public class 3Sum {
